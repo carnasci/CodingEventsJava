9e043f7a1e6f835bca9c7aae5c4d63cfb9221a6f
carnasci <cen.master24@gmail.com>
2024-01-19 19:39:00 -0600

    still need to add a table

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,10 +2,13 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,15 +18,36 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
+
+       /* List<String> events = new ArrayList<>();
         events.add("Code With Pride");
         events.add("Strange Loop");
         events.add("Apple WWDC");
         events.add("SpringOne Platform");
         model.addAttribute("events", events);
-        return "events/index";
+        return "events/index";*/
+
+        /*HashMap<String, String> events = new HashMap<>();
+        events.put("Menteahip", "A fun meetup for connecting with mentors");
+        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imagenary meetup for JavaScript developers");*/
+
+        model.addAttribute("events", events);
+        return "/events/index";
     }
 
+    @GetMapping("create")
+    public String renderCreateForm() {
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
 }
